65ff591abc16a6253ad3e8215e79df266a258a12
rval13v <livarfpv@gmail.com>
2025-10-24 21:58:14 +1100

    conftest

--- Tests/test_mail_2.py
+++ Tests/test_mail_2.py
@@ -1,14 +1,10 @@
 import pytest
 
 
-@pytest.fixture
-def set_up():
-    print("Вход в систему выполнен")
-
-def test_sednind_mail_1():
+def test_sendind_mail_1(set_up):
     print("Письмо отправлено")
 
 
-def test_sednind_mail_2():
+def test_sendind_mail_2(set_up):
     print("Письмо отправлено")
 
